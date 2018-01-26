963cb7fdfe48145f1ddcb069d68310a9304dd7a4
Mohamed Omar <jennmeedo@gmail.com>
2018-01-26 23:54:11 +0300

    Fixes to the templates

--- src/main/java/com/example/demo/controllers/HelloWorldController.java
+++ src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -1,14 +1,19 @@
 package com.example.demo.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
 
-	@RequestMapping("/hi")
-	public @ResponseBody String  SayHello() {
-		return "Hello There!!";
+	@GetMapping(path="/hi/{name}")
+	public  String  SayHello(Map model, @PathVariable String name) {
+		model.put("name", name);
+		return "hello";
 	}
 }
